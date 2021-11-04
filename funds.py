2b49529daaba2c3f2a282d04bc3dd10559fe4454
Xinyu Wang <739368858@qq.com>
2021-11-04 20:17:54 +0800

    update

--- funds.py
+++ funds.py
@@ -1,4 +1,14 @@
-def subtract_funds(account: str, amount: int):
+def subtract_funds1(account: str, amount: int):
+    '''Subtract funds from bank account then return; '''
+    bank[account] -= amount
+    return
+
+def subtract_funds2(account: str, amount: int):
     '''Subtract funds from bank account then ⁧''' ;return
     bank[account] -= amount
     return
+
+bank = {'alice': 100}
+
+subtract_funds2('alice', 50)
+print(bank)
